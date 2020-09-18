a4ee4ace295f03e9258bb147669a5172c37d965c
Renata-D <63861977+Renata-D@users.noreply.github.com>
2020-09-18 18:36:52 +0200

    Revert "Revert "Einbinden Vaadin""
    This reverts commit 2131d83a132868e7841a836bb2c7211e95f4305e.

---
+++ CarLock2/src/main/java/Entitaeten/Endkunde.java
@@ -0,0 +1,35 @@
+package Entitaeten;
+
+
+public class Endkunde extends User {
+	private String kundenid;
+	private String name;
+
+    
+
+    		public String getID() {
+        		return ID;
+    		}
+
+   		 public void setID(String kundenid) {
+        		this.kundeid = kundenid;
+    		}			
+
+    		public String getAnrede() {
+        		return anrede;
+    		}
+
+    		public void setAnrede(String anrede) {
+        		this.anrede = anrede;
+    		}
+
+    		public String name() {
+        		return name;
+    		}
+
+    		public void setVorname(String vorname) {
+        		this.vorname = vorname;
+    		}
+
+  
+}
